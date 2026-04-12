6ae720e31db20062cef14ca29f98281e94490a06
bobby-king3 <119807279+bobby-king3@users.noreply.github.com>
2026-04-11 22:08:17 -0500

    Initial dbt project files

---
+++ transform/dbt_project/models/staging/stg_play_by_play.sql
@@ -0,0 +1,24 @@
+with source as (
+    select * from raw_play_by_play
+)
+
+select
+    game_id,
+    event_id,
+    season,
+    period,
+    time_in_period,
+    event_type,
+    x_coord,
+    y_coord,
+    shot_type,
+    shooter_id,
+    goalie_id,
+    team_id,
+    situation_code,
+    home_defending_side,
+    away_score,
+    home_score,
+    highlight_clip_url,
+    raw
+from source
