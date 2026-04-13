7f5b5c8edbd042e37f5a6da4201351df091b24a0
bobby-king3 <119807279+bobby-king3@users.noreply.github.com>
2026-04-12 20:45:49 -0500

    Add moneypuck to int_shot_events

--- transform/dbt_project/models/intermediate/int_shot_events.sql
+++ transform/dbt_project/models/intermediate/int_shot_events.sql
@@ -2,6 +2,20 @@ with stg as (
     select * from {{ ref('stg_play_by_play') }}
 ),
 
+mp as (
+    -- Deduplicate MoneyPuck: saw rare cases where same player shoots twice in same second
+    select
+        mp_game_id,
+        shooter_id,
+        period,
+        seconds_elapsed,
+        max(x_goal)     as x_goal,
+        max(is_rush)    as is_rush,
+        max(is_rebound) as is_rebound
+    from {{ ref('stg_moneypuck_shots') }}
+    group by 1, 2, 3, 4
+),
+
 parsed as (
     select
         *,
@@ -9,10 +23,26 @@ parsed as (
         cast(substr(situation_code, 2, 1) as integer) as away_skaters,
         cast(substr(situation_code, 3, 1) as integer) as home_skaters,
         cast(substr(situation_code, 4, 1) as integer) as home_goalie,
-        case when x_coord >= 0 then 89.0 else -89.0 end as net_x
+        case when x_coord >= 0 then 89.0 else -89.0 end as net_x,
+        (cast(split_part(time_in_period, ':', 1) as integer) * 60
+            + cast(split_part(time_in_period, ':', 2) as integer)) as seconds_in_period
 
     from stg
     where x_coord is not null and y_coord is not null
+),
+
+joined as (
+    select
+        parsed.*,
+        mp.x_goal,
+        mp.is_rush,
+        mp.is_rebound
+    from parsed
+    left join mp
+        on cast(substring(cast(parsed.game_id as varchar), 5) as integer) = mp.mp_game_id
+        and parsed.shooter_id = mp.shooter_id
+        and parsed.period = mp.period
+        and ((parsed.period - 1) * 1200) + parsed.seconds_in_period = mp.seconds_elapsed
 ),
 
 final as (
@@ -22,8 +52,7 @@ final as (
         season,
         period,
         time_in_period,
-        (cast(split_part(time_in_period, ':', 1) as integer) * 60
-            + cast(split_part(time_in_period, ':', 2) as integer)) as seconds_in_period,
+        seconds_in_period,
         event_type,
         x_coord,
         y_coord,
@@ -54,9 +83,12 @@ final as (
             when away_skaters = home_skaters and away_skaters = 3 then '3v3'
             when away_skaters != home_skaters then 'PP/SH'
             else 'other'
-        end as strength
+        end as strength,
+        x_goal,
+        is_rush,
+        is_rebound
 
-    from parsed
+    from joined
 )
 
 select * from final
