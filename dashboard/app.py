eba76ba7eb7ab1f1bc2603a79390cd35136b12d2
bobby-king3 <119807279+bobby-king3@users.noreply.github.com>
2026-04-26 21:43:07 -0500

    Update player page name

--- dashboard/app.py
+++ dashboard/app.py
@@ -8,6 +8,6 @@ st.set_page_config(
 
 pg = st.navigation([
     st.Page("pages/teams.py",       title="Teams"),
-    st.Page("pages/player_card.py", title="Player Card")
+    st.Page("pages/player_card.py", title="Players")
 ])
 pg.run()
