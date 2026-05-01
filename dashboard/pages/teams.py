48d1f35a1b88f9b64df4768ad84d3206c434f2a8
bobby-king3 <119807279+bobby-king3@users.noreply.github.com>
2026-04-30 23:08:28 -0500

    moved team colors to utilities so it doesnt need to be repeated

--- dashboard/pages/teams.py
+++ dashboard/pages/teams.py
@@ -10,26 +10,7 @@ from dashboard.utils.db import (
 )
 from dashboard.utils.styling import hex_to_rgb
 from dashboard.utils.chart_builders import build_streak_dots_grid, build_team_rolling_xgpct
-
-TEAM_COLORS = {
-    "ANA": ("#FC4C02", "#B09862"), "BOS": ("#FFB81C", "#000000"),
-    "BUF": ("#FCB514", "#002654"), "CAR": ("#CC0000", "#000000"),
-    "CBJ": ("#CE1126", "#002654"), "CGY": ("#C8102E", "#F1BE48"),
-    "CHI": ("#CF0A2C", "#000000"), "COL": ("#6F263D", "#236192"),
-    "DAL": ("#006847", "#8F8F8C"), "DET": ("#CE1126", "#FFFFFF"),
-    "EDM": ("#FF4C00", "#041E42"), "FLA": ("#C8102E", "#041E42"),
-    "LAK": ("#A2AAAD", "#111111"), "MIN": ("#154734", "#A6192E"),
-    "MTL": ("#AF1E2D", "#192168"), "NJD": ("#CE1126", "#000000"),
-    "NSH": ("#FFB81C", "#041E42"), "NYI": ("#00539B", "#F47D30"),
-    "NYR": ("#0038A8", "#CE1126"), "OTT": ("#C8102E", "#C69214"),
-    "PHI": ("#F74902", "#000000"), "PIT": ("#FCB514", "#000000"),
-    "SEA": ("#99D9D9", "#001628"), "SJS": ("#006D75", "#EA7200"),
-    "STL": ("#FCB514", "#002F87"), "TBL": ("#1B5299", "#002868"),
-    "TOR": ("#2D7DD2", "#003E7E"), "UTA": ("#69B3E7", "#010101"),
-    "VAN": ("#00843D", "#00205B"), "VGK": ("#B4975A", "#333F42"),
-    "WSH": ("#C8102E", "#041E42"), "WPG": ("#004C97", "#041E42"),
-}
-DEFAULT_COLORS = ("#C8102E", "#1A1A2E")
+from dashboard.utils.colors import TEAM_COLORS, DEFAULT_COLORS
 
 TEAM_NAMES = {
     "ANA": "Anaheim Ducks", "BOS": "Boston Bruins", "BUF": "Buffalo Sabres",
