73ba29e16e075a179f5d468f1ae8a5002ef66395
Bobby King <119807279+bobby-king3@users.noreply.github.com>
2026-05-01 21:50:18 -0500

    Reviewed formatting in app.py

--- dashboard/app.py
+++ dashboard/app.py
@@ -7,7 +7,7 @@ st.set_page_config(
 )
 
 pg = st.navigation([
-    st.Page("pages/teams.py",       title="Teams"),
+    st.Page("pages/teams.py", title="Teams"),
     st.Page("pages/player_card.py", title="Players")
 ])
 pg.run()
